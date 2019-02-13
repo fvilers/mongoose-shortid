393013d99e565fdf992429aac13ea35e122cbfad
Fabian Vilers <fabian.vilers@dev-one.com>
2019-02-13 14:12:40 +0100

    Export decode & encode method

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ const base62 = require('./base62');
 const DEFAULT_OPTIONS = {
   propertyName: 'shortId'
 };
-const shortId = (schema, options) => {
+
+function encode(mongoId) {
+  return base62.encode(Buffer.from(mongoId.toString(), 'hex'));
+}
+
+function decode(shortId) {
+  return base62.decode(shortId).toString('hex');
+}
+
+function shortId(schema, options) {
   options = Object.assign(DEFAULT_OPTIONS, options);
 
   schema.virtual(options.propertyName).get(function() {
-    return base62.encode(Buffer.from(this._id.toString(), 'hex'));
+    return encode(this._id);
   });
-};
+}
 
 module.exports = shortId;
+module.exports.encode = encode;
+module.exports.decode = decode;
